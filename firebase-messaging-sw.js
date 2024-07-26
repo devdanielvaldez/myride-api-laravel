b6c2cd940b702602e4488bbc54d475bb5c0334a5
Daniel Valdez | FullStack Developer <68254937+devdanielvaldez@users.noreply.github.com>
2024-07-25 23:25:55 -0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,13 +2,14 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
 
 firebase.initializeApp({
-    apiKey: "AIzaSyBkFEj_PILbuy6JYDQLyBPiUHwg0Wej42c",
-    authDomain: "hexaride-fbdac.firebaseapp.com",
-    projectId: "hexaride-fbdac",
-    storageBucket: "hexaride-fbdac.appspot.com",
-    messagingSenderId: "30301959388",
-    appId: "1:30301959388:web:dc33ee586670882be57019",
-    measurementId: "G-ZTLK7ZL3TH"
+  apiKey: "AIzaSyBaGdE9AAEaXoAJbed4X8-btY7XmCDEGOY",
+  authDomain: "myride-capcana.firebaseapp.com",
+  databaseURL: "https://myride-capcana-default-rtdb.firebaseio.com",
+  projectId: "myride-capcana",
+  storageBucket: "myride-capcana.appspot.com",
+  messagingSenderId: "552752569147",
+  appId: "1:552752569147:web:0ea67235d0aa9e971bf51e",
+  measurementId: "G-T3CK7LG81J"
 });
 
 const messaging = firebase.messaging();
